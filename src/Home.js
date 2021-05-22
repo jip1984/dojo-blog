fb939bec4b7fd67495b01b2a74086173ca53387f
jon <jonprosser84@gmail.com>
2021-05-22 10:03:52 +0100

    changes

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import BlogList from './BlogList'
 
 const Home = () => {
+
+    // let name = 'mario';
+    const [blogs, setBlogs] = useState(null);
+
+    // const handleDelete = (id) => {
+    //     const newBlogs = blogs.filter(blog => blog.id !== id)
+
+    //     setBlogs(newBlogs);
+    // }
+
+    useEffect(() => {
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json()
+            })
+            .then((data) => {
+                console.log(data);
+                setBlogs(data)
+            })
+    }, []);
+
+
     return (
         <div className='home'>
-            <h1>Homepage</h1>
+
+            { blogs && <BlogList blogs={blogs} title="All Blogs!" />}
 
         </div>
     )
